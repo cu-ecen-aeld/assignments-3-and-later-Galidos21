b454545afcb06e51a0226694e4a4ea249717817d
Galidos <bryan.basantes21@outlook.com>
2025-04-02 21:54:54 -0500

    Assignment 4 Part 1 completed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -34,7 +34,8 @@ void* threadfunc(void* thread_param) {
 
     // Free memory and exit
     free(thread_func_args);
-    return NULL;
+    //return NULL;
+    return thread_param;
 }
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms) {
