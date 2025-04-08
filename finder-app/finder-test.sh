90c7bc81e66fe1a2a2d0d14a25a08e37675ba09c
Galidos <bryan.basantes21@outlook.com>
2025-04-08 00:09:17 -0500

    finder-test.sh modified for assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFIG_DIR=/etc/finder-app/conf
+username=$(cat ${CONFIG_DIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat ${CONFIG_DIR}/assignment.txt)
 
 if [ "$assignment" != 'assignment1' ]
 then
@@ -51,20 +52,25 @@ echo "Cleaning previous build artifacts and compiling writer application"
 
 for i in $(seq 1 $NUMFILES)
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
+# Capture the output of finder.sh into OUTPUTSTRING
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Write the output to a file
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}" > /tmp/assignment4-result.txt
 if [ $? -eq 0 ]; then
     echo "success"
     exit 0
 else
     echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    cat /tmp/assignment4-result.txt
     exit 1
 fi
